1488dc305c41ff696279dbed716b327d8dec8095
ketki kumeriya <111123802+Kumeriyakajal@users.noreply.github.com>
2023-10-12 13:17:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+() => {
+  cy.visit(baseUrl + "/main.html");
+  cy.get("div").then(span => {
+    const text = span.text().trim();
+    expect(text).to.eq("Welcome to the world of Web development");
+  });
+}
